40c68133bb10fc3319de1d89e4fd442fe216f6df
sashhw <sash.walk@gmail.com>
2021-07-15 11:16:15 -0500

    Initial Commit

---
+++ FirstTDD/FirstTDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTDDApp.swift
+//  FirstTDD
+//
+//  Created by Sasha on 7/15/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstTDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
